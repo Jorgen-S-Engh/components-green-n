f0cda006d7684514c1eb84a3088a79eab5940cf4
Jørgen Engh <jorgen.engh@hotmail.com>
2024-11-06 20:59:45 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,116 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Mail } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+// 00df81
+// 06302b
+
+// 032221
+// 0b453a
+
+// 03624c
+// 09544
+// f1f7f6
+//
+//#03624C
+//#021716
+//#01E081
+//
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="flex justify-center items-center m-5 flex-col gap-2">
+        <h4 className="text-center">Buttons</h4>
+        <Button variant="default">Button</Button>
+        <Button>
+          Kom i gang
+          <ArrowRight />
+        </Button>
+        <Button>
+          Kontakt oss
+          <Mail />
+        </Button>
+        <div className="flex gap-3">
+          <Button>Primary</Button>
+          <Button variant="secondary">Secondary</Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="mt-10 w-1/2">
+          <h4 className="text-center">Accordion</h4>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Du booker analyse av ditt bygg
+              </AccordionTrigger>
+              <AccordionContent>
+                Vi innhenter informasjon om ditt bygg, og analyserer data i våre
+                systemer
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Du booker analyse av ditt bygg
+              </AccordionTrigger>
+              <AccordionContent>
+                Vi innhenter informasjon om ditt bygg, og analyserer data i våre
+                systemer
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <div className="mb-56">
+          <h4 className="text-center">Select</h4>
+          <Select>
+            <SelectTrigger className="w-[180px] text-[#03624C]">
+              <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="bg-[#030F0E] w-full h-auto flex gap-7">
+          <div className="bg-[#06302b] w-1/2 p-10 rounded-lg m-5 flex flex-col items-center gap-5 h-[250px]">
+            <h5 className="text-[#f1f7f6] text-center text-lg font-bold">
+              Energisystemer sløser
+            </h5>
+            <p className="text-[#f1f7f6]">
+              og de er utfordrende å optimalisere Det er gjort store
+              investeringer i moderne norske bygg.
+            </p>
+            <Button className="w-32">Kontakt oss</Button>
+          </div>
+          <div className="bg-[#03624c] w-1/2 p-10 rounded-lg m-5 flex flex-col items-center gap-5 h-[250px]">
+            <h5 className="text-[#f1f7f6] text-center text-lg font-bold">
+              Energisystemer sløser
+            </h5>
+            <p className="text-[#f1f7f6]">
+              og de er utfordrende å optimalisere Det er gjort store
+              investeringer i moderne norske bygg.
+            </p>
+            <Button variant="secondary" className="w-32">
+              Kontakt oss
+            </Button>
+          </div>
+        </div>
+        <div className=" w-full h-[500px]"></div>
+      </div>
+    </>
   );
 }
